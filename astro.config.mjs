3c5ad9a14d8d977fb337fc7a99350b7126b55ec0
jasongith <jason_am95@hotmail.com>
2025-11-02 20:41:34 +0100

    cambios pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://jasongith.github.io/tertulia',
+  base: '/tertulia/',
   vite: {
     plugins: [tailwindcss()]
-  },
-  site: 'https://jasongith.github.io/',
-  base: '/tertulia/',
+  }
 });
